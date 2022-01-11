b4f24923aa1492dd2486db4e224281439fbb7fac
sidorovk <kirill@ankr.com>
2022-01-11 17:33:05 +0800

    Add sign and signature check

---
+++ Assets/Web3Unity/Scripts/Example/SignatureExample.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using Web3Unity.Scripts.Library;
+
+public class SignatureExample : MonoBehaviour
+{
+	private string message = "Hahaha!";
+	private string signature;
+	private Web3 web3;
+	
+	private void Start()
+	{
+		Debug.Log("---------------------- Start ----------------------");
+		string provider_url = "https://rinkeby.infura.io/v3/c75f2ce78a4a4b64aa1e9c20316fda3e";
+		
+		web3 = new Web3(provider_url);
+		web3.Initialize();
+	}
+	
+	public async void Sign1()
+	{
+		Debug.Log($"Signature: {signature}");
+	}
+
+	public async void Sign()
+	{
+		Debug.Log(web3);
+		signature = await web3.Sign(message);
+		Debug.Log($"Signature: {signature}");
+	}
+	
+	public void CheckSignature()
+	{
+		string address = web3.CheckSignature(message, signature);
+		Debug.Log($"Address from signature: {address}");
+	}
+}
--- Assets/Web3Unity/Scripts/Library/Web3.cs
+++ Assets/Web3Unity/Scripts/Library/Web3.cs
@@ -1,6 +1,9 @@
 using System;
+using System.Threading.Tasks;
 using Nethereum.JsonRpc.Client;
+using Nethereum.Signer;
 using Nethereum.Web3;
+using UnityEngine;
 using WalletConnectSharp.NEthereum;
 using WalletConnectSharp.Unity;
 
@@ -21,6 +24,8 @@ namespace Web3Unity.Scripts.Library
         public void Initialize()
         {
             // "https://rinkeby.infura.io/v3/c75f2ce78a4a4b64aa1e9c20316fda3e"
+            Debug.Log(WalletConnect.Instance);
+            Debug.Log(WalletConnect.Instance.Protocol);
             var wcProtocol = WalletConnect.Instance.Protocol;
             _client =
                 wcProtocol.CreateProvider(new Uri(_provider_url));
@@ -30,6 +35,19 @@ namespace Web3Unity.Scripts.Library
         public Contract GetContract(string address, string abi)
         {
             return new Contract(_provider, _client, address, abi);
+        }
+
+        public Task<string> Sign(string message)
+        {
+            Debug.Log("-------------------------- Hey men --------------------------");
+            Debug.Log($"Address: {WalletConnect.ActiveSession.Accounts[0]}");
+            return WalletConnect.ActiveSession.EthSign(WalletConnect.ActiveSession.Accounts[0], message);
+        }
+
+        public string CheckSignature(string message, string signature)
+        {
+            var signer1 = new EthereumMessageSigner();
+            return signer1.EncodeUTF8AndEcRecover(message, signature);
         }
     }
 }
