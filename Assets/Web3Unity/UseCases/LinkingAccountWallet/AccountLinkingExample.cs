64dde26edfd68e0688a67ffc708ce3eefb4d27bc
Stanley Wu <35579150+fruit37@users.noreply.github.com>
2022-01-16 13:40:54 -0800

    added the comments

--- Assets/Web3Unity/UseCases/LinkingAccountWallet/AccountLinkingExample.cs
+++ Assets/Web3Unity/UseCases/LinkingAccountWallet/AccountLinkingExample.cs
@@ -1,4 +1,4 @@
-﻿using System.Threading.Tasks;
+using System.Threading.Tasks;
 using Newtonsoft.Json;
 using UnityEngine;
 using UnityEngine.Networking;
@@ -17,15 +17,19 @@ public class RequestAnswer
 	public string Address;
 }
 
+// This example is to demonstrate how to bind the wallet into user account. 
 public class AccountLinkingExample : MonoBehaviour
 {
     [SerializeField]
+        // Message to be signed, which should be provided by the server
 	private string message = "Hahaha!";
 	private string signature;
 	private Web3 web3;
     [SerializeField]
-    private Text address;
+    	private Text address;
 
+	// A backend server to verfiy the ownership of this address using message and signature signed by 3rd party wallet
+	// an example can be found at https://github.com/mirage-xyz/mirage-go-demo/blob/main/main.go#L96
 	private string url = "http://2.56.91.78:8080/account/verification/address";
 	
 	private void Start()
@@ -33,23 +37,28 @@ public class AccountLinkingExample : MonoBehaviour
 		string provider_url = "https://rinkeby.infura.io/v3/c75f2ce78a4a4b64aa1e9c20316fda3e";
 		
 		web3 = new Web3(provider_url);
+		// Get the credential info from the 3rd party wallet
 		web3.Initialize();
 	}
-
+	
+	// function to sign the message
+	// step 1: sign the message with 3rd party wallet
+	// step 2: send the message and sign to the server 
+	// step 3: server return binded address 
 	public async void Sign()
 	{
 		signature = await web3.Sign(message);
 		Debug.Log($"Signature: {signature}");
-
+		
 		string address = await SendSignatue(signature);
 		Debug.Log($"Answer: {address}");
 
-        ShowAdressinUI();
+        	ShowAdressinUI();
 	}
 	
 	public void ShowAdressinUI()
 	{
-        address.text = signature;
+                address.text = signature;
 	}
 
 	private async Task<string> SendSignatue(string signature)
