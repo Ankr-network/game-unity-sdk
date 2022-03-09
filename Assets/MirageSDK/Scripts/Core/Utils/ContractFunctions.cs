ec0f22921750ed11ffaacb282e6cefbefa69dfa9
LUDO\Ludovic <ludovic.domingues96@gmail.com>
2022-03-09 10:09:47 +0100

    Added the new rental functions to erc 721

--- Assets/MirageSDK/Scripts/Core/Utils/ContractFunctions.cs
+++ Assets/MirageSDK/Scripts/Core/Utils/ContractFunctions.cs
@@ -2,6 +2,7 @@ using System.Numerics;
 using Cysharp.Threading.Tasks;
 using MirageSDK.Core.Infrastructure;
 using MirageSDK.Examples.ContractMessages.ERC721;
+using MirageSDK.Examples.ContractMessages.ERC721.RentableExtension;
 
 namespace MirageSDK.Core.Utils
 {
@@ -110,6 +111,34 @@ namespace MirageSDK.Core.Utils
 			};
 
 			return contract.GetData<TokenByIndexMessage, BigInteger>(tokenByIndexMessage).AsUniTask();
+		}
+
+		//THESE FUNCTIONS BELOW ARE ONLY USABLE BY ERC721 IMPLEMENTING THE RENTAL NFT EXTENSION
+
+		/// <summary>
+		///     Get the actual owner of the rented token "<paramref name="tokenId" />".
+		/// </summary>
+		public static UniTask<string> PrincipalOwner(string tokenId, IContract contract)
+		{
+			var principalOwnerMessage = new PrincipalOwnerMessage
+			{
+				TokenId = tokenId
+			};
+
+			return contract.GetData<PrincipalOwnerMessage, string>(principalOwnerMessage).AsUniTask();
+		}
+
+		/// <summary>
+		///     Get whether or not the token "<paramref name="tokenId" />" is rented.
+		/// </summary>
+		public static UniTask<bool> IsRented(string tokenId, IContract contract)
+		{
+			var isRentedMessage = new IsRentedMessage
+			{
+				TokenId = tokenId
+			};
+
+			return contract.GetData<IsRentedMessage, bool>(isRentedMessage).AsUniTask();
 		}
 
 		#endregion
@@ -236,6 +265,36 @@ namespace MirageSDK.Core.Utils
 			{
 				callOperator,
 				approved
+			}).AsUniTask();
+		}
+
+		//THESE FUNCTIONS BELOW ARE ONLY USABLE BY ERC721 IMPLEMENTING THE RENTAL NFT EXTENSION
+
+		/// <summary>
+		///     Rent a token to another address "<paramref name="renter" />" as an operator for the caller.
+		/// </summary>
+		public static UniTask<string> RentOut(string renter, string tokenId, BigInteger expiresAt, IContract contract)
+		{
+			const string rentOutMethodName = "rentOut";
+
+			return contract.CallMethod(rentOutMethodName, new object[]
+			{
+				renter,
+				tokenId,
+				expiresAt
+			}).AsUniTask();
+		}
+
+		/// <summary>
+		///     Renter can run this anytime but owner can run after the expired time.
+		/// </summary>
+		public static UniTask<string> FinishRenting(string tokenId, IContract contract)
+		{
+			const string finishRentingMethodName = "finishRenting";
+
+			return contract.CallMethod(finishRentingMethodName, new object[]
+			{
+				tokenId
 			}).AsUniTask();
 		}
 
