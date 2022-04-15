40a51c4b0924b2c4864ab9ca852190bad5815f23
LUDO\Ludovic <ludovic.domingues96@gmail.com>
2022-04-15 09:42:11 +0200

    Revert "Fixed hardcoded address recipient issue"
    This reverts commit 807d680a7fdeacf5a5c514a1a553cc5251b797fe.

--- Assets/AnkrSDK/Examples/Scripts/WearableNFTExample/WearableNFTExample.cs
+++ Assets/AnkrSDK/Examples/Scripts/WearableNFTExample/WearableNFTExample.cs
@@ -88,6 +88,7 @@ namespace AnkrSDK.WearableNFTExample
 		private async UniTask MintItems()
 		{
 			const string mintBatchMethodName = "mintBatch";
+			const string mintBatchToAddress = "0x510c522ebCC6Eb376839E0CFf5D57bb2F422EB8b";
 			var itemsToMint = new[]
 			{
 				BlueHatAddress,
@@ -104,7 +105,7 @@ namespace AnkrSDK.WearableNFTExample
 			var data = new byte[] { };
 
 			var receipt = await _gameItemContract.CallMethod(mintBatchMethodName,
-				new object[] {_activeSessionAccount, itemsToMint, itemsAmounts, data});
+				new object[] {mintBatchToAddress, itemsToMint, itemsAmounts, data});
 
 			UpdateUILogs($"Game Items Minted. Receipts : {receipt}");
 		}
