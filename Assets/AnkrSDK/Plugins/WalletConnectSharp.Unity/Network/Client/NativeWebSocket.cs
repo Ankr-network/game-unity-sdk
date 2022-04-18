4279331e6694435e288e521f9e92e7ad5f5c5f7b
Eugene Alekseev <alekseev@ankr.com>
2022-04-18 15:32:51 +0400

    wip

--- Assets/AnkrSDK/Plugins/WalletConnectSharp.Unity/Network/Client/NativeWebSocket.cs
+++ Assets/AnkrSDK/Plugins/WalletConnectSharp.Unity/Network/Client/NativeWebSocket.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net.WebSockets;
-using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -60,112 +59,6 @@ namespace NativeWebSocket
         event WebSocketCloseEventHandler OnClose;
 
         WebSocketState State { get; }
-    }
-
-    public static class WebSocketHelpers
-    {
-        public static WebSocketCloseCode ParseCloseCodeEnum(int closeCode)
-        {
-            if (Enum.IsDefined(typeof(WebSocketCloseCode), closeCode))
-            {
-                return (WebSocketCloseCode) closeCode;
-            }
-
-            return WebSocketCloseCode.Undefined;
-        }
-
-        public static WebSocketException GetErrorMessageFromCode(int errorCode, Exception inner)
-        {
-            switch (errorCode)
-            {
-                case -1:
-                    return new WebSocketUnexpectedException("WebSocket instance not found.", inner);
-                case -2:
-                    return new WebSocketInvalidStateException(
-                        "WebSocket is already connected or in connecting state.", inner);
-                case -3:
-                    return new WebSocketInvalidStateException("WebSocket is not connected.", inner);
-                case -4:
-                    return new WebSocketInvalidStateException("WebSocket is already closing.", inner);
-                case -5:
-                    return new WebSocketInvalidStateException("WebSocket is already closed.", inner);
-                case -6:
-                    return new WebSocketInvalidStateException("WebSocket is not in open state.", inner);
-                case -7:
-                    return new WebSocketInvalidArgumentException(
-                        "Cannot close WebSocket. An invalid code was specified or reason is too long.", inner);
-                default:
-                    return new WebSocketUnexpectedException("Unknown error.", inner);
-            }
-        }
-    }
-
-    public class WebSocketException : Exception
-    {
-        public WebSocketException()
-        {
-        }
-
-        public WebSocketException(string message) : base(message)
-        {
-        }
-
-        public WebSocketException(string message, Exception inner) : base(message, inner)
-        {
-        }
-    }
-
-    public class WebSocketUnexpectedException : WebSocketException
-    {
-        public WebSocketUnexpectedException()
-        {
-        }
-
-        public WebSocketUnexpectedException(string message) : base(message)
-        {
-        }
-
-        public WebSocketUnexpectedException(string message, Exception inner) : base(message, inner)
-        {
-        }
-    }
-
-    public class WebSocketInvalidArgumentException : WebSocketException
-    {
-        public WebSocketInvalidArgumentException()
-        {
-        }
-
-        public WebSocketInvalidArgumentException(string message) : base(message)
-        {
-        }
-
-        public WebSocketInvalidArgumentException(string message, Exception inner) : base(message, inner)
-        {
-        }
-    }
-
-    public class WebSocketInvalidStateException : WebSocketException
-    {
-        public WebSocketInvalidStateException()
-        {
-        }
-
-        public WebSocketInvalidStateException(string message) : base(message)
-        {
-        }
-
-        public WebSocketInvalidStateException(string message, Exception inner) : base(message, inner)
-        {
-        }
-    }
-
-    public class WaitForBackgroundThread
-    {
-        public ConfiguredTaskAwaitable.ConfiguredTaskAwaiter GetAwaiter()
-        {
-            return Task.Run(() => { }).ConfigureAwait(false).GetAwaiter();
-        }
     }
 
 #if UNITY_WEBGL && !UNITY_EDITOR
