d0d326b253ec87b55d039bc0b73b659b70076ee8
alekseeveg <alekseev@ankr.com>
2022-07-11 15:24:41 +0400

    ads scene fixed

--- Assets/AnkrSDK/Scripts/Mobile/AnkrNetworkHelper.cs
+++ Assets/AnkrSDK/Scripts/Mobile/AnkrNetworkHelper.cs
@@ -1,39 +1,38 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using AnkrSDK.Core.Infrastructure;
 using AnkrSDK.Data;
+using AnkrSDK.Utils;
 using Newtonsoft.Json;
 using UnityEngine;
 
-namespace AnkrSDK.Utils
+namespace AnkrSDK.Mobile
 {
 	public class AnkrNetworkHelper : INetworkHelper
 	{
-		private static readonly string _networkQueryParameter = "network";
-		private static readonly string _logoQueryParameter = "logo";
-		private static readonly string _deepLinkURL = "https://metamask.app.link/dapp";
-		private static readonly string _switchNetworkPageLink = "changenetwork-service.game.ankr.com";
+		private const string NetworkQueryParameter = "network";
+		private const string LogoQueryParameter = "logo";
+		private const string DeepLinkURL = "https://metamask.app.link/dapp";
+		private const string SwitchNetworkPageLink = "changenetwork-service.game.ankr.com";
 
 		public Task AddAndSwitchNetwork(EthereumNetwork network)
 		{
 			var url = GetURLFromNetwork(network);
-			AddAndSwitchCustomNetwork(url);
-			
+			OpenCustomNetworkSwitchURL(url);
 			return Task.CompletedTask;
 		}
 
-		public static void AddAndSwitchCustomNetwork(string url)
+		private static void OpenCustomNetworkSwitchURL(string url)
 		{
 			Application.OpenURL(url);
 		}
 
 		private static string GetURLFromNetwork(EthereumNetwork network, string logoIcon = null)
 		{
-			var uriQueryBuilder = new UriQueryBuilder($"{_deepLinkURL}/{_switchNetworkPageLink}");
-			uriQueryBuilder.AppendArgument(_networkQueryParameter, JsonConvert.SerializeObject(network));
+			var uriQueryBuilder = new UriQueryBuilder($"{DeepLinkURL}/{SwitchNetworkPageLink}");
+			uriQueryBuilder.AppendArgument(NetworkQueryParameter, JsonConvert.SerializeObject(network));
 			if (logoIcon != null)
 			{
-				uriQueryBuilder.AppendArgument(_logoQueryParameter, logoIcon);
+				uriQueryBuilder.AppendArgument(LogoQueryParameter, logoIcon);
 			}
 
 			return uriQueryBuilder.Build();
