6dd59865dc9ca11ab501f15684dfbc78211500c3
Ludovic_Domingues <ludovic.domingues96@gmail.com>
2022-08-18 13:52:30 +0200

    added sieing to billboard ad + updated backend info

--- Assets/AnkrSDK/Examples/UseCases/Ads/AdsBackendInformations.cs
+++ Assets/AnkrSDK/Examples/UseCases/Ads/AdsBackendInformations.cs
@@ -2,8 +2,8 @@ namespace AnkrSDK.UseCases.Ads
 {
 	public static class AdsBackendInformation
 	{
-		public const string FullscreenAdTestUnitId = "2592a670-f7ab-4400-ab95-af7d5975a9f8";
-		public const string BannerAdTestUnitId = "cc758a98-bb29-4628-89c2-4fa5294ccf28";
-		public const string TestAppId = "9e6624ba-0653-4230-86b8-204bddca8a8f";
+		public const string FullscreenAdTestUnitId = "test_fullscreen";
+		public const string BannerAdTestUnitId = "test_banner";
+		public const string TestAppId = "test_app";
 	}
 }
