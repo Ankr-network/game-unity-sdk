017bfbc35e6ccec290889783df6f2ff3db5ac763
Anton Vikharev <anton.vikharev@decentraland.org>
2022-12-22 12:33:45 +0400

    old export method removed

--- Assets/Editor/PackageExporter.cs
+++ Assets/Editor/PackageExporter.cs
@@ -4,21 +4,13 @@ using UnityEditor;
 
 public static class PackageExporter
 {
-	private const string PackageName = "AnkrSDK";
 	private const string ImporterPackageName = "AnkrSDKImporter";
 
 	// The path to the package under the `Assets/` folder.
-	private const string PackagePath = "Assets/AnkrSDK";
 	private const string ImporterPackagePath = "Assets/AnkrSDKImporter";
 
 	// Path to export to.
 	private const string ExportPath = "Build";
-
-	[MenuItem("AnkrSDK/Export Ankr Package")]
-	public static void Export()
-	{
-		ExportPackage($"{ExportPath}/{PackageName}.unitypackage", PackagePath);
-	}
 
 	[MenuItem("AnkrSDK/Export Ankr Importer Package")]
 	public static void ExportImporter()
