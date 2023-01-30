a310b0de3b6497898abd5d981b44d54dc652d3ec
Anton Vikharev <anton.vikharev@decentraland.org>
2023-01-30 14:13:44 +0400

    webglconnect bug fixes

--- Assets/AnkrSDK/Examples/UseCases/WebGlLogin/WebGLConnectionController.cs
+++ Assets/AnkrSDK/Examples/UseCases/WebGlLogin/WebGLConnectionController.cs
@@ -1,5 +1,4 @@
 using AnkrSDK.Data;
-using AnkrSDK.Utils;
 using AnkrSDK.WebGL;
 using UnityEngine;
 
@@ -9,23 +8,56 @@ namespace AnkrSDK.Examples.UseCases.WebGlLogin
 	{
 		[SerializeField]
 		private WebGLLoginPanelController _webGlLoginManager;
-		
+
 		[SerializeField]
 		private GameObject _sceneChooser;
-		private WebGLConnect WebGLConnect => ConnectProvider<WebGLConnect, WebGLConnectSettingsSO>.GetConnect();
+
+		private WebGLConnect WebGLConnect
+		{
+			get
+			{
+#if (UNITY_WEBGL && !UNITY_EDITOR)
+				return ConnectProvider<WebGLConnect, WebGLConnectSettingsSO>.GetConnect();
+#else
+				return null;
+#endif
+			}
+		}
 
 		private void Awake()
 		{
-			WebGLConnect.OnNeedPanel += ActivatePanel;
-			WebGLConnect.OnConnect += ChangeLoginPanel;
-			_webGlLoginManager.NetworkChosen += OnNetworkChosen;
-			_webGlLoginManager.WalletChosen += OnWalletChosen;
+			if (WebGLConnect != null)
+			{
+				WebGLConnect.OnNeedPanel += ActivatePanel;
+				WebGLConnect.OnConnect += ChangeLoginPanel;
+				_webGlLoginManager.NetworkChosen += OnNetworkChosen;
+				_webGlLoginManager.WalletChosen += OnWalletChosen;
+			}
+			else
+			{
+				gameObject.SetActive(false);
+			}
 		}
 
 		private async void Start()
 		{
 			_sceneChooser.SetActive(false);
-			await WebGLConnect.Connect();
+			if(WebGLConnect != null)
+			{
+				await WebGLConnect.Connect();
+			}
+		}
+
+		private void OnDisable()
+		{
+			var webGlConnect = WebGLConnect;
+			if (webGlConnect != null)
+			{
+				webGlConnect.OnNeedPanel -= ActivatePanel;
+				webGlConnect.OnConnect -= ChangeLoginPanel;
+			}
+			_webGlLoginManager.NetworkChosen -= OnNetworkChosen;
+			_webGlLoginManager.WalletChosen -= OnWalletChosen;
 		}
 
 		private void ActivatePanel()
@@ -33,7 +65,7 @@ namespace AnkrSDK.Examples.UseCases.WebGlLogin
 			_webGlLoginManager.ShowPanel();
 		}
 
-		private void ChangeLoginPanel(WebGL.WebGLWrapper provider)
+		private void ChangeLoginPanel(WebGLWrapper provider)
 		{
 			_webGlLoginManager.HidePanel();
 			_sceneChooser.SetActive(true);
@@ -41,20 +73,12 @@ namespace AnkrSDK.Examples.UseCases.WebGlLogin
 
 		private void OnNetworkChosen(NetworkName network)
 		{
-			WebGLConnect.SetNetwork(network);
-		}
-		
-		private void OnWalletChosen(Wallet wallet)
-		{
-			WebGLConnect.SetWallet(wallet);
+			WebGLConnect?.SetNetwork(network);
 		}
 
-		private void OnDisable()
+		private void OnWalletChosen(Wallet wallet)
 		{
-			WebGLConnect.OnNeedPanel -= ActivatePanel;
-			WebGLConnect.OnConnect -= ChangeLoginPanel;
-			_webGlLoginManager.NetworkChosen -= OnNetworkChosen;
-			_webGlLoginManager.WalletChosen -= OnWalletChosen;
+			WebGLConnect?.SetWallet(wallet);
 		}
 	}
 }
