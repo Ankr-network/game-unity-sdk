2892c84814324d946aa26e6dbd25a25e29e007e2
Anton Vikharev <anton.vikharev@decentraland.org>
2023-02-28 19:13:52 +0300

    minor fix

--- Assets/AnkrSDK/Runtime/WalletConnect2/RpcRequests/WalletUpdateEthChainData.cs
+++ Assets/AnkrSDK/Runtime/WalletConnect2/RpcRequests/WalletUpdateEthChainData.cs
@@ -5,8 +5,8 @@ using WalletConnectSharp.Network.Models;
 namespace AnkrSDK.WalletConnect2.RpcRequests
 {
 	[RpcMethod("wallet_updateEthereumChain")]
-	[RpcRequestOptions(Clock.SIX_HOURS, false, 2016)]
-	[RpcResponseOptions(Clock.SIX_HOURS, false, 2017)]
+	[RpcRequestOptions(Clock.SIX_HOURS, false, 2017)]
+	[RpcResponseOptions(Clock.SIX_HOURS, false, 2018)]
 	public class WalletUpdateEthChainData : RpcRequestListDataBase
 	{
 		public WalletUpdateEthChainData(EthUpdateChainData chainData)
