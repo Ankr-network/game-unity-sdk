e2ceb300133d462e63b1f892dba0e90457f2642f
Anton Vikharev <anton@mirage.xyz>
2023-03-13 17:02:34 +0300

    GitHub action to update version in a file (#105)
    * github action to modify package json
    
    * minor fix
    
    * handle missing tag case
    
    * typo fix
    
    * yaml syntax fix
    
    * autocommit changes
    
    * git commit commands modification
    
    * git commit command modification
    
    * git commit command modification
    
    * update commit command
    
    * git commit and push command modified
    
    * silence all safe.directory warnings
    
    * git commands update
    
    * upgrade git push action version
    
    * update pushed tag
    
    * tags usage removed
    
    * set tags to false
    
    * run workflow on git push
    
    * add git status command
    
    * git diff index command added
    
    * if else check improved
    
    * create file
    
    * push to branch
    
    * another attempt to push to branch
    
    * more logs
    
    * branch name reading improvement
    
    * get branch name different approach
    
    * commit and push tag update to a new branch
    
    * push in a different way
    
    * push tag separately
    
    * move tag to a new commit
    
    * tag update workflow improvements
    
    * move a git tag to a current commit
    
    * git action workflow polished
    
    * additional log
    
    * additional checkout to be not in a detatched head state
    
    * add fetch command
    
    * add stashing commands for checkouts
    
    * git add command to stage changes for commit
    
    * Automated tag update for v0.5.5
    
    * permission related typo fix
    
    * rever version
    
    * specify ref
    
    * remove unused line
    
    * Automated tag update for v0.5.5
    
    * do things in different order
    
    * revert tag
    
    * commands order issue fix
    
    * Automated tag update for
    
    * bash variable update
    
    * revert package json
    
    * Automated tag update for v0.5.5
    
    * push tag alternitively
    
    * Revert "Automated tag update for v0.5.5"
    
    This reverts commit 12f5643b11eb1a9469e778b9381b299f2ab3ed8b.
    
    * Automated tag update for v0.5.5
    
    * typo fix
    
    * Revert "Automated tag update for v0.5.5"
    
    This reverts commit 3dd9531204f0966803e0821589d0132863b6338c.
    
    * Automated tag update for v0.5.5
    
    * different way to move tag
    
    * Revert "Automated tag update for v0.5.5"
    
    This reverts commit 741f8a4fff11ac7fba7efee01a5e9d9ddab2e400.
    
    * Automated tag update for v0.5.5
    
    * alternative way to move tag
    
    * Revert "Automated tag update for v0.5.5"
    
    This reverts commit 4596733cfea5788b52bd3b89b8411e6548f70cdb.
    
    * Automated tag update for v0.5.5
    
    * tag a particular commit
    
    * Revert "Automated tag update for v0.5.5"
    
    This reverts commit 55bcd4c3c8d69067cf29d6d8ad26998eafd0ace2.
    
    * Automated tag update for v0.5.5
    
    * more detailed logging
    
    * Revert "Automated tag update for v0.5.5"
    
    This reverts commit a4e5b5397551c6c41ad1c247c3fd890323bd5a19.
    
    * Automated tag update for v0.5.5
    
    * Revert "Automated tag update for v0.5.5"
    
    This reverts commit 0fb938281692e48d29a23df3248994c2040c2386.
    
    * better tag name filter
    
    * improved commit name
    
    * minor renaming
    
    * move py script to different folder
    
    * Automated package.json version update for tag v0.5.5
    
    * Revert "Automated package.json version update for tag v0.5.5"
    
    This reverts commit aba73ac9859dda22bb6f0dbd2ed5f64019c2757b.
    
    ---------
    
    Co-authored-by: GitHub Actions <actions@github.com>

---
+++ .github/python_scripts/modify_package_version_based_on_git_tag.py
@@ -0,0 +1,36 @@
+import subprocess
+import json
+import sys
+
+package_file_path = 'Assets/AnkrSDK/package.json'
+
+commit_hash = subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode().strip()
+
+try:
+    git_tag = subprocess.check_output(['git', 'describe', '--exact-match', '--tags', commit_hash]).decode().strip()
+except subprocess.CalledProcessError:
+    print('git tag was not found for commit ' + commit_hash)
+    sys.exit(0)
+
+if git_tag and len(git_tag) > 0:
+
+    new_package_version = git_tag.replace("v", "")
+
+    with open(package_file_path, 'r') as f:
+        # Load the JSON object into a dictionary
+        data = json.load(f)
+
+    prev_package_version = data['version'] if 'version' in data else 'none'
+    data['version'] = new_package_version
+
+    if new_package_version != prev_package_version:
+        with open(package_file_path, 'w') as f:
+            # Dump the modified dictionary back to the file
+            json.dump(data, f, indent=2)
+
+        print(f'version changed from {prev_package_version} to {new_package_version}')
+    else:
+        print(f'version {new_package_version} did not change')
+
+else:
+    print('git tag was none or empty for commit ' + commit_hash)
