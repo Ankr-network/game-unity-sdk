00ce5de875a6d7d15e6a7d98c01cccd01f0ac0e3
Anton Vikharev <anton.vikharev@decentraland.org>
2023-03-20 22:27:53 +0300

    format file

--- Assets/AnkrSDK/Runtime/Utils/EventAwaiter.cs
+++ Assets/AnkrSDK/Runtime/Utils/EventAwaiter.cs
@@ -6,75 +6,74 @@ using Cysharp.Threading.Tasks;
 
 namespace AnkrSDK.Utils
 {
-	public class EventAwaiter<T> where T : EventDTOBase, new()
-	{
-		private readonly string _contractAddress;
-		private EventFilterRequest<T> _filter;
-		private readonly IContractEventSubscriber _eventSubscriber;
-		private UniTaskCompletionSource<T> _receiveEventCompletionSource;
-		private T _resultEventDto;
-		private IContractEventSubscription _subscription;
-		
-		public UniTask<T> ReceiveEventTask
-		{
-			get
-			{
-				if (_resultEventDto != null)
-				{
-					return UniTask.FromResult(_resultEventDto);
-				}
-				
-				if (_receiveEventCompletionSource == null)
-				{
-					throw new InvalidOperationException("Waiting for event was not started");
-				}
+    public class EventAwaiter<TEventDto> where TEventDto : EventDTOBase, new()
+    {
+        private readonly string _contractAddress;
+        private readonly IContractEventSubscriber _eventSubscriber;
 
-				return _receiveEventCompletionSource.Task;
-			}
-		}
+        private IContractEventSubscription _subscription;
+        private EventFilterRequest<TEventDto> _filter;
+        private UniTaskCompletionSource<TEventDto> _receiveEventCompletionSource;
+        private TEventDto _resultEventDto;
 
-		public EventAwaiter(string contractAddress, string wsProviderURL)
-		{
-			_contractAddress = contractAddress;
-			_eventSubscriber = new ContractEventSubscriber(wsProviderURL);
-			
-		}
+        public UniTask<TEventDto> ReceiveEventTask
+        {
+            get
+            {
+                if (_resultEventDto != null)
+                {
+                    return UniTask.FromResult(_resultEventDto);
+                }
 
-		public async UniTask StartWaiting(EventFilterRequest<T> filtersRequest)
-		{
-			_receiveEventCompletionSource = new UniTaskCompletionSource<T>();
-			_eventSubscriber.ListenForEvents().Forget();
-			await _eventSubscriber.SocketOpeningTask;
-			
-			try
-			{
-				_subscription = await _eventSubscriber.Subscribe(
-					filtersRequest,
-					_contractAddress,
-					Callback
-				);
-			}
-			catch (Exception e)
-			{
-				_receiveEventCompletionSource.TrySetException(e);
-			}
-			
-		}
+                if (_receiveEventCompletionSource == null)
+                {
+                    throw new InvalidOperationException("Waiting for event was not started");
+                }
 
-		private void Callback(T eventDto)
-		{
-			try
-			{
-				_resultEventDto = eventDto;
-				_eventSubscriber.Unsubscribe(_subscription.SubscriptionId);
-			}
-			catch (Exception e)
-			{
-				_receiveEventCompletionSource.TrySetException(e);
-				return;
-			}
-			
-			_receiveEventCompletionSource.TrySetResult(eventDto);
-		}
-	}
+                return _receiveEventCompletionSource.Task;
+            }
+        }
+
+        public EventAwaiter(string contractAddress, string wsProviderURL)
+        {
+            _contractAddress = contractAddress;
+            _eventSubscriber = new ContractEventSubscriber(wsProviderURL);
+        }
+
+        public async UniTask StartWaiting(EventFilterRequest<TEventDto> filtersRequest)
+        {
+            _receiveEventCompletionSource = new UniTaskCompletionSource<TEventDto>();
+            _eventSubscriber.ListenForEvents().Forget();
+            await _eventSubscriber.SocketOpeningTask;
+
+            try
+            {
+                _subscription = await _eventSubscriber.Subscribe(
+                    filtersRequest,
+                    _contractAddress,
+                    Callback
+                );
+            }
+            catch (Exception e)
+            {
+                _receiveEventCompletionSource.TrySetException(e);
+            }
+        }
+
+        private void Callback(TEventDto eventDto)
+        {
+            try
+            {
+                _resultEventDto = eventDto;
+                _eventSubscriber.Unsubscribe(_subscription.SubscriptionId);
+            }
+            catch (Exception e)
+            {
+                _receiveEventCompletionSource.TrySetException(e);
+                return;
+            }
+
+            _receiveEventCompletionSource.TrySetResult(eventDto);
+        }
+    }
 }
