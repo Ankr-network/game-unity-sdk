be84255bbe8d1ba41954a28e3721bcecadc5bc50
Anton VIkharev <anton@mirage.xyz>
2023-04-13 12:48:31 +0300

    preparation python script extended

--- PythonScripts/prepare_for_asset_store.py
+++ PythonScripts/prepare_for_asset_store.py
@@ -2,6 +2,7 @@ import os
 import shutil
 import json
 
+
 def remove_dir(path):
     """
     Recursively remove directory and its contents.
@@ -9,12 +10,14 @@ def remove_dir(path):
     if os.path.exists(path):
         shutil.rmtree(path)
 
+
 def remove_file(path):
     """
     Remove a file at the given path.
     """
     if os.path.exists(path):
         os.remove(path)
+
 
 def remove_assets_and_folders(project_path):
     """
@@ -45,30 +48,42 @@ def remove_assets_and_folders(project_path):
         remove_file(resources_dir + ".meta")
     else:
         print("failed to remove " + resources_dir)
-        
+
+
+def put_sdk_folder_to_mirage_folder(project_path):
+    mirage_sdk_dir = os.path.join(project_path, "Assets", "MirageSDK")
+
+    if os.path.exists(mirage_sdk_dir):
+        mirage_dir = os.path.join(project_path, "Assets", "Mirage")
+        remove_dir(mirage_dir)
+        os.mkdir(mirage_dir)
+        shutil.move(mirage_sdk_dir, mirage_dir)
+        remove_file(mirage_sdk_dir + ".meta")
+
+
 def remove_packages(project_path):
     remove_packages = ["com.unity.mobile.android-logcat", "com.unity.test-framework", "com.unity.ide.rider"]
-    
+
     # Remove package references from manifest.json and packages-lock.json files
     for file_name in ["manifest.json", "packages-lock.json"]:
         file_path = os.path.join(project_path, "Packages", file_name)
         with open(file_path, "r") as file:
             data = json.load(file)
-        
+
         for package in remove_packages:
             if package in data["dependencies"]:
                 print(f"Removing package from {file_name}:", package)
                 data[f"dependencies"].pop(package, None)
-        
+
         with open(file_path, "w") as file:
             json.dump(data, file, indent=4)
-    
+
+
 if __name__ == "__main__":
-    
     file_path = os.path.abspath(__file__)
     dir_path = os.path.dirname(file_path)
     parent_dir_path = os.path.abspath(os.path.join(dir_path, os.pardir))
-    
-    remove_assets_and_folders(parent_dir_path)
-    remove_packages(parent_dir_path)
 
+    remove_assets_and_folders(parent_dir_path)
+    put_sdk_folder_to_mirage_folder(parent_dir_path)
+    remove_packages(parent_dir_path)
