5ee14f1254653d992defce1d1432a24aa21a7e63
Anton VIkharev <anton@mirage.xyz>
2023-04-26 14:22:55 +0300

    generic icon loading with resources folder as a source

--- Assets/MirageSDK/Plugins/WalletConnect.VersionShared/Models/DeepLink/Helpers/WalletDataHelper.cs
+++ Assets/MirageSDK/Plugins/WalletConnect.VersionShared/Models/DeepLink/Helpers/WalletDataHelper.cs
@@ -1,8 +1,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Newtonsoft.Json;
-using UnityEngine;
 
 namespace MirageSDK.WalletConnect.VersionShared.Models.DeepLink.Helpers
 {
@@ -32,14 +30,13 @@ namespace MirageSDK.WalletConnect.VersionShared.Models.DeepLink.Helpers
 		/// </summary>
 		/// <param name="walletName"></param>
 		/// <returns></returns>
-		public static string GetOverrideUrl(string walletName)
+		public static string GetOverrideImageUrl(string walletName)
 		{
 			switch (walletName)
 			{
 				case "MRG Wallet":
 				{
-					return
-						"https://play-lh.googleusercontent.com/VxEmZKRr4z44UfWdx8LkjMHPfBDgUQ4k_GKvsRvrIPe377TKpBJsRzXwRvAhDrb0BEw=w240-h480-rw";
+					return "resources:icon-mrg-wallet";
 				}
 			}
 
--- Assets/MirageSDK/Plugins/WalletConnect.VersionShared/Models/DeepLink/WalletEntry.cs
+++ Assets/MirageSDK/Plugins/WalletConnect.VersionShared/Models/DeepLink/WalletEntry.cs
@@ -1,8 +1,6 @@
-using System;
 using Cysharp.Threading.Tasks;
 using MirageSDK.WalletConnect.VersionShared.Models.DeepLink.Helpers;
 using UnityEngine;
-using UnityEngine.Networking;
 
 namespace MirageSDK.WalletConnect.VersionShared.Models.DeepLink
 {
@@ -29,62 +27,39 @@ namespace MirageSDK.WalletConnect.VersionShared.Models.DeepLink
 
             foreach (var size in sizes)
             {
-                var urlOverride = WalletDataHelper.GetOverrideUrl(name);
-                string url = null;
+                var urlOverride = WalletDataHelper.GetOverrideImageUrl(name);
+                string uri = null;
                 if (urlOverride != null)
                 {
-                    url = urlOverride;
+                    uri = urlOverride;
                 }
                 else
                 {
-                    url = "https://registry.walletconnect.org/logo/" + size + "/" + id + ".jpeg";
+                    uri = "https://registry.walletconnect.org/logo/" + size + "/" + id + ".jpeg";
                 }
 
-                using (var imageRequest = UnityWebRequestTexture.GetTexture(url))
+                var texture = await WebHelper.GetTextureFromGenericUri(uri);
+
+                if (texture == null)
                 {
-                    try
-                    {
-                        await imageRequest.SendWebRequest();
-                    }
-                    catch (UnityWebRequestException e)
-                    {
-                        var downloadHandler = ((DownloadHandlerTexture)imageRequest.downloadHandler);
-                        Debug.LogError($"Exception while loading texture {e.Message} download handler error: {downloadHandler.error}");
-                        return;
-                    }
+                    return;
+                }
+                
+                var sprite = Sprite.Create(texture,
+                    new Rect(0.0f, 0.0f, texture.width, texture.height),
+                    new Vector2(0.5f, 0.5f), 100.0f);
 
-#if UNITY_2020_2_OR_NEWER
-                    if (imageRequest.result != UnityWebRequest.Result.Success)
-                    {
-                        Debug.Log("Error Getting Wallet Icon: " + imageRequest.error);
-                    }
-#else
-                        if (imageRequest.isHttpError || imageRequest.isNetworkError)
-                        {
-                            Debug.Log("Error Getting Wallet Icon: " + imageRequest.error);
-                        }
-#endif
-                    else
-                    {
-                        var downloadHandler = ((DownloadHandlerTexture)imageRequest.downloadHandler);
-                        var texture = downloadHandler.texture;
-                        var sprite = Sprite.Create(texture,
-                            new Rect(0.0f, 0.0f, texture.width, texture.height),
-                            new Vector2(0.5f, 0.5f), 100.0f);
-
-                        switch (size)
-                        {
-                            case "sm":
-                                SmallIcon = sprite;
-                                break;
-                            case "md":
-                                MediumIcon = sprite;
-                                break;
-                            case "lg":
-                                LargeIcon = sprite;
-                                break;
-                        }
-                    }
+                switch (size)
+                {
+                    case "sm":
+                        SmallIcon = sprite;
+                        break;
+                    case "md":
+                        MediumIcon = sprite;
+                        break;
+                    case "lg":
+                        LargeIcon = sprite;
+                        break;
                 }
             }
         }
